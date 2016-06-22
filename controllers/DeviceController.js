eb44f8672214adba750e54b60fb1adc8216940c2
bin.shen <bin.shen@openx.com>
2016-06-22 13:32:00 +0800

    111

--- controllers/DeviceController.js
+++ controllers/DeviceController.js
@@ -7,9 +7,9 @@ var Common = require('../utils/common');
 module.exports = function (app, mongoose, config) {
     var Device = mongoose.model('Device');
 
-    app.post('/device/get_list',function(req, res, next) {
-        var userID = req.body.userID;
-        Device.find({ userID: userID }, function(err, doc) {
+    app.get('/device/mac/:mac/get',function(req, res, next) {
+        var mac = req.params.mac;
+        Device.find({ mac: mac }, function(err, doc) {
             if(err) return next(err);
             return res.status(200).json(doc);
         });
