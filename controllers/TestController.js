68fc52befd2260b92e9031f5a044b3c43ad4f826
bin.shen <bin.shen@openx.com>
2016-06-24 00:44:09 +0800

    111

--- controllers/TestController.js
+++ controllers/TestController.js
@@ -8,6 +8,22 @@ module.exports = function (app, mongoose, config) {
     var User = mongoose.model('User');
     var Device = mongoose.model('Device');
 
+    app.get('/user/register', function(req, res, next) {
+        var username = "13999999999";
+        var password = Common.md5("888888");
+        User.findOne({username: username}, function(err, doc) {
+            if(err) return next(err);
+            if(doc != null) {
+                return res.status(400).json({ success:false, error:"该手机号码已经注册过" });
+            }
+            doc = new User({ username: username, password: password });
+            doc.save(function(err) {
+                if(err) return next(err);
+                return res.status(200).json({ success:true });
+            });
+        });
+    });
+
     app.get('/test/login',function(req, res, next) {
         var username = "13999999999";
         var password = Common.md5("888888");
