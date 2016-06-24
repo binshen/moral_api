dbb325d15cbe7b3c74a1fcf6f2885cfc8d21c524
bin.shen <bin.shen@openx.com>
2016-06-25 01:05:19 +0800

    111

--- controllers/DeviceController.js
+++ controllers/DeviceController.js
@@ -17,21 +17,21 @@ module.exports = function (app, mongoose, config) {
         });
     });
 
-    app.get('/device/mac/:mac/get_current_data',function(req, res, next) {
-        var mac = req.params.mac;
-        //var today = moment().startOf('day');
-        //var tomorrow = moment(today).add(1, 'days');
-        Data.find({
-            mac: mac,
-            day: moment().format('YYYYMMDD'),
-            //created: { $gte: today.valueOf(), $lt: tomorrow.valueOf() }
-        }).sort({'created': -1}).limit(1).exec(function(err, doc) {
-            if(err) return next(err);
-            return res.status(200).json(doc);
-        });
-    });
+    // app.get('/device/mac/:mac/get_current',function(req, res, next) {
+    //     var mac = req.params.mac;
+    //     //var today = moment().startOf('day');
+    //     //var tomorrow = moment(today).add(1, 'days');
+    //     Data.find({
+    //         mac: mac,
+    //         day: moment().format('YYYYMMDD'),
+    //         //created: { $gte: today.valueOf(), $lt: tomorrow.valueOf() }
+    //     }).sort({'created': -1}).limit(1).exec(function(err, doc) {
+    //         if(err) return next(err);
+    //         return res.status(200).json(doc);
+    //     });
+    // });
 
-    app.get('/device/mac/:mac/get_history_data',function(req, res, next) {
+    app.get('/device/mac/:mac/get_history',function(req, res, next) {
         var mac = req.params.mac;
         var day = req.query.day;
         Data.find({
