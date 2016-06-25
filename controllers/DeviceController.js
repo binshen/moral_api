6e4102f5a4e9cce6b4c67386da89c9bae6b87d12
bin.shen <bin.shen@openx.com>
2016-06-25 23:18:25 +0800

    111

--- controllers/DeviceController.js
+++ controllers/DeviceController.js
@@ -31,13 +31,39 @@ module.exports = function (app, mongoose, config) {
     //     });
     // });
 
-    app.get('/device/mac/:mac/get_history',function(req, res, next) {
+    app.get('/device/mac/:mac/get_history2',function(req, res, next) {
         var mac = req.params.mac;
         var day = req.query.day;
         Data.findOne({
             mac: mac,
             day: day
-        }).sort({'created': -1}).limit(1).exec(function(err, doc) {
+        }).limit(1).exec(function(err, doc) {
+            if(err) return next(err);
+            return res.status(200).json(doc == null ? {} : doc);
+        });
+    });
+
+    app.get('/device/mac/:mac/get_history',function(req, res, next) {
+        var mac = req.params.mac;
+        var day = req.query.day;
+        Data.aggregate([
+            {
+                $match: {
+                    mac: mac,
+                    day : day
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    x1:  { $avg: "$x1" },
+                    x3:  { $avg: "$x3" },
+                    x9:  { $avg: "$x9" },
+                    x10: { $avg: "$x10" },
+                    x11: { $avg: "$x11" }
+                }
+            }
+        ]).exec(function(err, doc) {
             if(err) return next(err);
             return res.status(200).json(doc == null ? {} : doc);
         });
--- test.js
+++ test.js
@@ -6,6 +6,8 @@
 // var day = moment(1466610063007);
 // console.log(day.format());
 
-var random = require("random-js")(); // uses the nativeMath engine
-var value = random.integer(100000, 999999);
-console.log(value)
+// var random = require("random-js")(); // uses the nativeMath engine
+// var value = random.integer(100000, 999999);
+// console.log(value)
+
+console.log(0x96);
