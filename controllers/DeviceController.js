0510dcd2d764b115baa7be4cefb672b07bf930ea
bin.shen <bin.shen@openx.com>
2016-06-25 23:20:07 +0800

    111

--- controllers/DeviceController.js
+++ controllers/DeviceController.js
@@ -65,7 +65,7 @@ module.exports = function (app, mongoose, config) {
             }
         ]).exec(function(err, doc) {
             if(err) return next(err);
-            return res.status(200).json(doc == null ? {} : doc[0]);
+            return res.status(200).json(doc == null || doc.length < 1 ? {} : doc[0]);
         });
     });
 };
