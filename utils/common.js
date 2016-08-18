0d3f460e4eeed69242b45dc288345f1f3fbed87e
bin.shen <bin.shen@openx.com>
2016-08-18 23:08:24 +0800

    123

--- utils/common.js
+++ utils/common.js
@@ -11,7 +11,8 @@ module.exports.md5 = function(value) {
 
 module.exports.sendMsg = function(tel, msg, callback) {
     var msg = encodeURIComponent(msg);
-    var url = "http://send.18sms.com/msg/HttpBatchSendSM?account=002011&pswd=XUwei10051&mobile="+tel+"&msg="+msg+"&needstatus=true";
+    //var url = "http://send.18sms.com/msg/HttpBatchSendSM?account=002011&pswd=XUwei10051&mobile="+tel+"&msg="+msg+"&needstatus=true";
+    var url = "http://120.26.66.24/msg/HttpBatchSendSM?account=mred_hy&pswd=mred_hy123&product=191991231&mobile="+tel+"&msg="+msg+"&needstatus=true";
     request(url, function (err, resp, body) {
         if(err) return callback(err);
         callback(null, body);
