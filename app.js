45c4a4110d4bef077603a0aa00d1a1c3391b7c91
bin.shen <bin.shen@openx.com>
2016-08-20 10:17:00 +0800

    123

--- app.js
+++ app.js
@@ -22,12 +22,10 @@ app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
+    res.header("X-Powered-By",' 3.2.1');
     res.header("Content-Type", "application/json;charset=utf-8");
     next();
 });
-
-
 
 // var logDirectory = path.join(__dirname, 'log');
 // fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
