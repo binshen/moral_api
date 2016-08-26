946097c25dcea5fa1e62fcb6099d2b6f33de35c7
bin.shen <bin.shen@openx.com>
2016-08-26 17:15:21 +0800

    12345

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -11,7 +11,8 @@ module.exports = function (app, mongoose, config) {
                 { name: 'moral_android_app' } ,
                 { version: 2 },
                 { message: "检测到新版本，需要立即更新吗?" },
-                { url: 'http://121.40.92.176/apps/app-release.apk' }
+                //{ url: 'http://121.40.92.176/apps/app-release.apk' }
+                { url: 'http://www.7drlb.com/apps/app-release.apk' }
             ]
         }];
         res.set('Content-Type', 'text/xml');
